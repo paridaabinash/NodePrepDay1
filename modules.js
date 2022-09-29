7211349e0e2274209be265f21a9cb4fa38a12390
paridaabinash <99640788+paridaabinash@users.noreply.github.com>
2022-09-29 13:29:13 +0530

    Create modules.js

---
+++ modules.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    console.log(a+b)
+}
+function substract(a,b){
+    console.log(a-b)
+}
+function multiply(a,b){
+    console.log(a*b)
+}
+const divide=(a,b)=>{
+    console.log(a/b)
+}
+
+
+
+module.exports={add,substract,multiply,divide};
